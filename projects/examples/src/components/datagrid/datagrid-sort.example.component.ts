42c676aa949828cfad1f4031df9d2cec788881cd
Ryan Bradford <ryanbradford333@gmail.com>
2020-02-12 15:07:23 -0500

    Merge sorting for testing purposes. (#32)
    * Added sorting capabilities
    
    Signed-off-by: Ryan Bradford <rbradford@vmware.com>
    
    * Slight name changes
    
    Signed-off-by: Ryan Bradford <rbradford@vmware.com>
    
    * Field name + comment changes
    
    Signed-off-by: Ryan Bradford <rbradford@vmware.com>

---
+++ projects/examples/src/components/datagrid/datagrid-sort.example.component.ts
@@ -0,0 +1,69 @@
+/*!
+ * Copyright 2020 VMware, Inc.
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+import { Component } from '@angular/core';
+import { GridDataFetchResult, GridColumn, GridState } from '@vcd/ui-components';
+
+interface Data {
+    value: string;
+    someBool: boolean;
+}
+
+/**
+ * A component that holds an example of the sorting columns capability.
+ */
+@Component({
+    selector: 'vcd-datagrid-sort-example',
+    template: `
+        <vcd-datagrid [gridData]="gridData" (gridRefresh)="refresh($event)" [columns]="columns"> </vcd-datagrid>
+    `,
+})
+export class DatagridSortExampleComponent {
+    gridData: GridDataFetchResult<Data> = {
+        items: [],
+    };
+
+    columns: GridColumn<Data>[] = [
+        {
+            displayName: 'Column',
+            renderer: 'value',
+        },
+        {
+            displayName: 'Boolean',
+            renderer: 'someBool',
+        },
+    ];
+
+    refresh(eventData: GridState<Data>): void {
+        let data = [
+            { value: 'a', someBool: true },
+            { value: 'b', someBool: false },
+            { value: 'c', someBool: true },
+            { value: 'a', someBool: false },
+            { value: 'f', someBool: true },
+            { value: 'c', someBool: true },
+        ];
+        if (eventData.sortColumn) {
+            if (eventData.sortColumn.name === 'Column') {
+                data = data.sort((a, b) => a.value.localeCompare(b.value));
+                if (eventData.sortColumn.reverse) {
+                    data = data.reverse();
+                }
+            }
+            if (eventData.sortColumn.name === 'Boolean') {
+                data = data.sort((a, b) => (a.someBool === b.someBool ? 0 : a.someBool ? -1 : 1));
+                if (eventData.sortColumn.reverse) {
+                    data = data.reverse();
+                }
+            }
+        }
+        this.gridData = {
+            items: data,
+            totalItems: 2,
+            pageSize: 2,
+            page: 1,
+        };
+    }
+}
